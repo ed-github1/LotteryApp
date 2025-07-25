7291b9c8f2eaa62ba0a6fd85e3459b410d926776
Edgar Tapia! <edghartapia@gmail.com>
2025-07-24 22:09:59 -0600

    first test for client

--- LotteryApp-backend/app.js
+++ LotteryApp-backend/app.js
@@ -5,6 +5,7 @@ import connectToDatabase from './utils/db.js'
 import cors from 'cors'
 import usersRouter from './controllers/users.js'
 import ordersRouter from './controllers/order.js'
+import winnerNumberRouter from './controllers/winnerNumber.js'
 const app = express()
 connectToDatabase()
 app.use(express.json())
@@ -13,14 +14,14 @@ app.use(morgan('dev'))
 //
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://lotteryapp.netlify.com',
     credentials: true
   })
 )
 app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/orders', ordersRouter)
-
+app.use('/api/winner-number', winnerNumberRouter)
 export default app
 // This is the main application file for the Lottery App backend.
 // It sets up the Express server, middleware, and a basic route.
